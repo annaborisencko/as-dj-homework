5210404a306273c2d33373f890d4e1f29225c85f
Anna Sidorenko <annaborisencko@yandex.ru>
2025-10-06 14:00:34 +0300

    Task 2.2.2 additional is done

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -1,12 +1,13 @@
 from django.views.generic import ListView
 from django.shortcuts import render
+from django.db.models import Prefetch
 
-from .models import Student
+from .models import Student, Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    students = Student.objects.all()
+    students = Student.objects.prefetch_related(Prefetch('teachers', queryset=Teacher.objects.all(), to_attr='teacher_list'))
     context = {
         'object_list': students
     }
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -22,7 +22,9 @@ SECRET_KEY = 'b0e@^m&tccz11$w59qov$lhn-97!(%wfn-gray-c*x)^a$wx=2'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 SITE_ID = 1
 
@@ -36,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'school',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
