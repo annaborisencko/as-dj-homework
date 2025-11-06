aaebab80597c30756b272c354f581fd3397e8117
Anna Sidorenko <annaborisencko@yandex.ru>
2025-11-06 11:53:57 +0300

    Task 3.2-crud is done

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -3,14 +3,28 @@ from rest_framework.viewsets import ModelViewSet
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet, NumberFilter, CharFilter, ModelMultipleChoiceFilter
 
+class ProductFilter(FilterSet):
+    products = NumberFilter(field_name="products__id")
+    
+    class Meta:
+        model = Stock
+        fields = ['products']
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+
+    filterset_class = ProductFilter
+    search_fields = ['products__title', 'products__description']
